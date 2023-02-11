1640ced3f2da8000b1d7d9cd12b158141dd17538
syoch <syoch64@gmail.com>
2023-02-11 09:57:56 +0900

    Update: Remove some codes

--- src/wasm/wasm/src/main.rs
+++ src/wasm/wasm/src/main.rs
@@ -1,44 +1,11 @@
-use kernel::Handle;
-
 extern crate kernel;
-extern crate python;
+// extern crate python;
 
-mod processes;
+// mod processes;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut k = kernel::Kernel::default();
-    let p = processes::IPCMaster::default();
-    k.register_process(Box::new(p));
 
-    let p = processes::IPCSlave::default();
-    k.register_process(Box::new(p));
     k.start();
     Ok(())
 }
-/*
-#[cfg(test)]
-mod test {
-    use python::PythonProcess;
-
-    #[test]
-    fn python_based_process() {
-        let mut k = kernel::Kernel::default();
-        let p = PythonProcess::new(
-            r#"async def proc():
-        print("print")
-        print("/")
-
-        print("step")
-        await step()
-
-        print("pending")
-        await pending()"#
-                .to_string(),
-        )
-        .unwrap();
-        k.register_process(Box::new(p));
-        k.start();
-    }
-}
-
-*/
