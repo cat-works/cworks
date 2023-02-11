93527947b93e1972c36c0dd332c7b34ac02c84ac
syoch <syoch64@gmail.com>
2023-02-11 11:18:01 +0900

    Update: rewrite some parts

---
+++ src/AppBar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import app from "./libs/app";
+</script>
+
+<div class="container" />
+
+<style>
+  .container {
+    height: 100%;
+    width: 100%;
+    background-color: white;
+    border: 1px solid lightgray;
+    border-radius: 3px;
+
+    padding: 5px 10px;
+  }
+</style>
