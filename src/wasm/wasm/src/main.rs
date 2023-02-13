832e74e21a7e3116711bd9be0cc669a7761798bc
syoch <syoch64@gmail.com>
2023-02-13 11:40:29 +0900

    some

--- src/wasm/wasm/src/main.rs
+++ src/wasm/wasm/src/main.rs
@@ -1,25 +1,61 @@
 use std::sync::Arc;
 
-use kernel::SyscallError;
+use kernel::{Handle, SyscallError};
 use rust_process::{RustProcess, Session};
 
 extern crate kernel;
 // extern crate python;
 
-async fn p(session: Arc<Session>) -> Result<i64, SyscallError> {
+async fn server(session: Arc<Session>) -> Result<i64, SyscallError> {
     let s = session.ipc_create("aiueo".to_string()).await?;
     println!("Server: {}", s);
+    let mut sc: Option<Arc<Handle>> = None; // ServerClient
+
+    while let data = session.get_syscall_data().await {
+        match data {
+            kernel::SyscallData::Connection { client, server } => {
+                println!("Connection");
+                println!("  Client: {}", client);
+                println!("  Server: {}", server);
+
+                if server == s {
+                    sc = Option::Some(client);
+                }
+            }
+            kernel::SyscallData::ReceivingData { focus, data }
+                if Option::Some(focus.clone()) == sc =>
+            {
+                println!("Received: {} [Client -> Server]", data);
+
+                session
+                    .ipc_send(focus.clone(), "DAT-KITAYO".to_string())
+                    .await?;
+                break;
+            }
+            _ => {
+                println!("{data:?}");
+                panic!();
+            }
+        }
+    }
+
+    Ok(0i64)
+}
+
+async fn client(session: Arc<Session>) -> Result<i64, SyscallError> {
+    session.sleep(0.5).await;
     let c = session.ipc_connect("aiueo".to_string()).await?;
+    session
+        .ipc_send(c.clone(), "Helloworld".to_string())
+        .await?;
+
     println!("Client: {}", c);
 
-    while let data = session.get_syscall_data() {
+    while let data = session.get_syscall_data().await {
         match data {
-            kernel::SyscallData::ReceivingData { focus, data } if focus == s => {
-                println!("Received: {} [Server]", data);
-            }
-
             kernel::SyscallData::ReceivingData { focus, data } if focus == c => {
-                println!("Received: {} [Client]", data);
+                println!("Received: {} [Client -> Server]", data);
+                break;
             }
 
             _ => {
@@ -35,8 +71,8 @@ async fn p(session: Arc<Session>) -> Result<i64, SyscallError> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut k = kernel::Kernel::default();
 
-    let p = RustProcess::new(&p);
-    k.register_process(Box::new(p));
+    k.register_process(Box::new(RustProcess::new(&server)));
+    k.register_process(Box::new(RustProcess::new(&client)));
 
     k.start();
     Ok(())
