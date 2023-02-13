d66fed2f5dd71ae93eba1ddd5642fdbd9dbafc00
syoch <syoch64@gmail.com>
2023-02-13 09:38:52 +0900

    Update: Implemented Into<i64> for SyscallError

--- src/wasm/kernel/src/process/syscall_error.rs
+++ src/wasm/kernel/src/process/syscall_error.rs
@@ -9,6 +9,18 @@ pub enum SyscallError {
 
     UnreachableEntry,
 }
+
+impl Into<i64> for SyscallError {
+    fn into(self) -> i64 {
+        match self {
+            SyscallError::NoSuchEntry => -1,
+            SyscallError::AlreadyExists => -2,
+            SyscallError::UnknownHandle => -3,
+            SyscallError::NotImplemented => -4,
+            SyscallError::UnreachableEntry => -5,
+        }
+    }
+}
 impl std::fmt::Display for SyscallError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "{:?}", self)
