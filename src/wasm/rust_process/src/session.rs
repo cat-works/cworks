aa81df12e6106fd31f84ecb90dc498a63520eebc
syoch <syoch64@gmail.com>
2023-02-13 11:40:16 +0900

    Update: some changes

--- src/wasm/rust_process/src/session.rs
+++ src/wasm/rust_process/src/session.rs
@@ -1,4 +1,7 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+};
 
 use kernel::{Handle, Syscall, SyscallData, SyscallError};
 
@@ -7,6 +10,8 @@ use crate::dummy_future::DummyFuture;
 pub struct Session {
     pub(crate) syscall: Arc<Mutex<Option<Syscall>>>,
     pub(crate) syscall_data: Arc<Mutex<SyscallData>>,
+
+    data_buffer: Mutex<VecDeque<Arc<SyscallData>>>,
 }
 
 impl Default for Session {
@@ -16,57 +21,93 @@ impl Default for Session {
         Self {
             syscall: syscall,
             syscall_data: data,
+            data_buffer: VecDeque::new().into(),
         }
     }
 }
 
 impl Session {
-    fn set_syscall(&self, syscall: Syscall) {
-        *self.syscall.lock().unwrap() = Some(syscall);
-    }
-    fn return_handle(&self) -> Result<Arc<Handle>, SyscallError> {
+    fn poll_syscall_data(&self) {
         let m = self.syscall_data.lock().unwrap();
         match &(*m) {
-            SyscallData::Handle(r) => {
-                return r.clone();
-            }
+            SyscallData::None => {}
             _ => {
-                println!("{:?}", m);
-                panic!("unexpected syscall data");
+                self.data_buffer
+                    .lock()
+                    .unwrap()
+                    .push_back(Arc::new((*m).clone()));
             }
         }
     }
 
-    pub fn get_syscall_data(&self) -> SyscallData {
-        self.syscall_data.lock().unwrap().clone()
+    fn set_syscall(&self, syscall: Syscall) {
+        *self.syscall.lock().unwrap() = Some(syscall);
+    }
+
+    async fn return_handle(&self) -> Result<Arc<Handle>, SyscallError> {
+        loop {
+            {
+                let mut buffer = self.data_buffer.lock().unwrap();
+                match buffer.pop_front() {
+                    Some(x) => match *x {
+                        SyscallData::Handle(ref e) => return e.clone(),
+                        _ => {
+                            buffer.push_back(x);
+                        }
+                    },
+                    None => {}
+                }
+            }
+
+            self.poll_syscall_data();
+            DummyFuture::Started.await;
+        }
+    }
+
+    pub async fn get_syscall_data(&self) -> SyscallData {
+        loop {
+            let f = self
+                .data_buffer
+                .lock()
+                .unwrap()
+                .pop_front()
+                .map(|x| (*x).clone());
+            if let Some(x) = f {
+                return x;
+            } else {
+                self.poll_syscall_data();
+                DummyFuture::Started.await;
+            }
+        }
+    }
+
+    pub async fn sleep(&self, seconds: f32) {
+        self.set_syscall(Syscall::Sleep(seconds));
+        DummyFuture::Started.await;
     }
 
     pub async fn ipc_create(&self, name: String) -> Result<Arc<Handle>, SyscallError> {
         self.set_syscall(Syscall::IpcCreate(name));
         DummyFuture::Started.await;
-        self.return_handle()
+        self.return_handle().await
     }
+
     pub async fn ipc_send(&self, handle: Arc<Handle>, data: String) -> Result<(), SyscallError> {
         self.set_syscall(Syscall::Send(handle, data));
         DummyFuture::Started.await;
 
-        let m = self.syscall_data.lock().unwrap();
-        match &(*m) {
-            SyscallData::None => {
-                return Ok(());
-            }
-            SyscallData::Handle(Err(e)) => {
+        match *(self.syscall_data.lock().unwrap()) {
+            SyscallData::Handle(Err(ref e)) => {
+                self.set_syscall_data(&SyscallData::None);
                 return Err(e.clone());
             }
-            _ => {
-                panic!("unexpected syscall data");
-            }
+            _ => Ok(()),
         }
     }
     pub async fn ipc_connect(&self, name: String) -> Result<Arc<Handle>, SyscallError> {
         self.set_syscall(Syscall::IpcConnect(name));
         DummyFuture::Started.await;
-        self.return_handle()
+        self.return_handle().await
     }
 
     pub(crate) fn set_syscall_data(&self, data: &SyscallData) {
