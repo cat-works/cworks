130750ff99a4b013e7488a0b4bd745edee08890e
syoch <syoch64@gmail.com>
2023-02-13 09:26:27 +0900

    wip

--- src/wasm/wasm/src/main.rs
+++ src/wasm/wasm/src/main.rs
@@ -1,10 +1,25 @@
+use std::sync::Arc;
+
+use rust_process::{RustProcess, Session};
+
 extern crate kernel;
 // extern crate python;
 
-// mod processes;
+async fn p(session: Arc<Session>) -> i64 {
+    let a = session.ipc_create("aiueo".to_string()).await;
+    let n = session.ipc_create("neptune".to_string()).await;
+
+    println!("a: {}", a);
+    println!("n: {}", n);
+
+    0i64
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut k = kernel::Kernel::default();
+
+    let p = RustProcess::new(&p);
+    k.register_process(Box::new(p));
 
     k.start();
     Ok(())
