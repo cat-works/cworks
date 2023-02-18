e2cb245c09233bc9fcb4bd41e068bdf96e4b55a9
syoch <syoch64@gmail.com>
2023-02-19 08:08:32 +0900

    SOME

--- src/wasm/kernel/src/kernel_processes/mod.rs
+++ src/wasm/kernel/src/kernel_processes/mod.rs
@@ -1,6 +1,10 @@
+mod path;
 use std::{
+    borrow::{Borrow, BorrowMut},
+    cell::RefCell,
     collections::HashMap,
     ops::{Deref, DerefMut},
+    rc::Rc,
     sync::{Arc, Mutex},
 };
 
@@ -10,6 +14,8 @@ enum FSReturns {
     InvalidCommandFormat,
     UnsupportedMethod,
     InvalidHandle,
+    UnknownPath,
+    Ok,
 }
 
 impl From<FSReturns> for String {
@@ -18,85 +24,80 @@ impl From<FSReturns> for String {
             FSReturns::InvalidCommandFormat => "InvalidCommandFormat".to_string(),
             FSReturns::UnsupportedMethod => "UnsupportedMethod".to_string(),
             FSReturns::InvalidHandle => "InvalidHandle".to_string(),
+            FSReturns::UnknownPath => "UnknownPath".to_string(),
+            FSReturns::Ok => "Ok".to_string(),
         }
     }
 }
 
 enum FSCommand {
     List,
+    Cd(String),
 }
 
 impl TryFrom<String> for FSCommand {
     type Error = FSReturns;
     fn try_from(value: String) -> Result<Self, FSReturns> {
-        match value.as_str() {
-            "List" => Ok(FSCommand::List),
+        if value == "" {
+            return Err(FSReturns::InvalidCommandFormat);
+        }
+
+        let toks = value.split("?").collect::<Vec<_>>();
+        match (toks.len(), toks[0]) {
+            (1, "List") => Ok(FSCommand::List),
+            (2, "Cd") => Ok(FSCommand::Cd(toks[1].to_string())),
             _ => Err(FSReturns::InvalidCommandFormat),
         }
     }
 }
 
 struct FS {
-    root: Arc<Mutex<FSObj>>,
+    pub root: FSObj,
 }
 
 impl FS {
-    pub fn new(root: Arc<Mutex<FSObj>>) -> Self {
+    pub fn new(root: FSObj) -> Self {
         Self { root }
     }
-
-    pub fn cursor(&self) -> FSCursor {
-        return FSCursor::new("/".to_string(), self.root.clone());
-    }
-}
-
-impl Deref for FS {
-    type Target = Arc<Mutex<FSObj>>;
-    fn deref(&self) -> &Self::Target {
+    pub(crate) fn get_obj(&self) -> &FSObj {
         &self.root
     }
-}
-
-impl DerefMut for FS {
-    fn deref_mut(&mut self) -> &mut Self::Target {
+    pub(crate) fn get_obj_mut(&mut self) -> &mut FSObj {
         &mut self.root
     }
-}
 
-struct FSCursor {
-    path: String,
-    obj: Arc<Mutex<FSObj>>,
-}
-
-impl FSCursor {
-    pub fn new(path: String, obj: Arc<Mutex<FSObj>>) -> Self {
-        Self { path, obj }
+    pub fn exists(&self, path: String) -> bool {
+        self.root.get_obj(path).is_ok()
     }
 
-    pub fn list(&self) -> Result<Vec<String>, FSReturns> {
-        match *self.obj.lock().unwrap() {
-            FSObj::Dist(ref map) => Ok(map.keys().map(|x| x.to_string()).collect::<Vec<_>>()),
+    pub fn list(&self, path: String) -> Result<Vec<String>, FSReturns> {
+        match self
+            .root
+            .get_obj(path)
+            .map_err(|_| FSReturns::UnknownPath)?
+        {
+            FSObj::Dist(x) => Ok(x.keys().map(|x| x.clone()).collect::<Vec<_>>()),
             _ => Err(FSReturns::UnsupportedMethod),
         }
     }
 }
 
-pub async fn fs_daemon_process(
-    session: Arc<Session>,
-    daemon: Arc<Mutex<FSObj>>,
-) -> Result<i64, SyscallError> {
+pub async fn fs_daemon_process(session: Arc<Session<FSObj>>) -> Result<i64, SyscallError> {
     let _s = session.ipc_create("system/file-system".to_string()).await?;
 
-    let fs = FS::new(daemon.clone());
+    let fs = session.get_value();
+    let fs = FS::new(fs);
+
     let mut state = HashMap::new();
 
     loop {
         let data = session.get_syscall_data().await;
         match data {
             SyscallData::Connection { client, server: _ } => {
-                state.insert(client.id, fs.cursor());
+                state.insert(client.id, "/".to_string());
             }
             SyscallData::ReceivingData { focus, data } => {
+                println!("Received: Client[{}] -> Server: {}", focus.id, data);
                 let r = match FSCommand::try_from(data.clone()) {
                     Ok(x) => x,
                     Err(e) => {
@@ -105,33 +106,30 @@ pub async fn fs_daemon_process(
                     }
                 };
 
-                match r {
-                    FSCommand::List => {
-                        let state = match state.get(&focus.id) {
-                            Some(x) => x,
-                            None => {
-                                session
-                                    .ipc_send(focus.clone(), "InvalidHandle".to_string())
-                                    .await?;
-                                continue;
-                            }
-                        };
-                        let ret = match state.list().map(|x| x.join(", ")) {
-                            Ok(x) => x,
-                            Err(e) => {
-                                session.ipc_send(focus.clone(), e.into()).await?;
-                                continue;
-                            }
-                        };
-
-                        session.ipc_send(focus.clone(), ret).await?;
-                    }
-                }
-
+                let ret = match r {
+                    FSCommand::List => match state.get(&focus.id).map(|x| fs.list(x.to_string())) {
+                        Some(Ok(x)) => x.join("?"),
+                        Some(Err(e)) => e.into(),
+                        None => "InvalidHandle".to_string(),
+                    },
+                    FSCommand::Cd(path) => match state
+                        .get(&focus.id)
+                        .map(|x| path::join(x, path.clone()))
+                        .map(|x| (x.clone(), fs.exists(x)))
+                    {
+                        Some((p, f)) if f == true => {
+                            state.insert(focus.id, p);
+                            "Success".to_string()
+                        }
+                        Some((_, f)) if f == false => "UnknownPath".to_string(),
+                        Some(_) => "UnknownError".to_string(),
+                        None => "InvalidHandle".to_string(),
+                    },
+                };
                 println!("Received: {} [Client -> Server]", data);
-
-                break;
+                session.ipc_send(focus.clone(), ret).await?;
             }
+
             _ => {
                 println!("{data:?}");
                 panic!();
