0cf4a4540848249ce85ef424e810b5738461c2a8
syoch <syoch64@gmail.com>
2023-02-18 10:03:24 +0900

    update: some

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,5 @@
+import { writable } from "svelte/store";
+
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
@@ -6,4 +8,38 @@ export function my_stringify(x: object) {
   return JSON.stringify(x, (_, v) =>
     typeof v === "bigint" ? v.toString() : v
   );
+}
+eval("window.my_stringify = my_stringify");
+
+export function timestamp(): string {
+  return new Date().toISOString();
+}
+
+export function patch_console_log() {
+  let buf = writable("");
+
+  eval("console.clog = console.log");
+
+  let clog = console.log;
+  window.console.log = (...args) => {
+    clog(...args);
+    const time = timestamp();
+    const data = args
+      .map((arg) => {
+        if (!arg) {
+          return "null";
+        }
+
+        if (typeof arg === "string") {
+          return arg;
+        }
+
+        return my_stringify(arg);
+      })
+      .join(" ");
+
+    buf.update(x => `${x}\n${time} ${data}`);
+  };
+
+  return buf;
 }
