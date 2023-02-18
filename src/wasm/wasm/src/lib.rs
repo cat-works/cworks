0ace61d82629c801a030bfad819aa8602c7442ba
syoch <syoch64@gmail.com>
2023-02-18 09:24:52 +0900

    Update: Added Handle cacher

--- src/wasm/wasm/src/lib.rs
+++ src/wasm/wasm/src/lib.rs
@@ -6,9 +6,7 @@ extern crate ghs_demangle;
 use std::collections::HashMap;
 
 pub use generator::generate_user_id;
-use kernel::{
-    Handle, HandleData, HandleIssuer, PollResult, Process, Syscall, SyscallData, SyscallError,
-};
+use kernel::{Handle, PollResult, Process, Syscall, SyscallData};
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
 #[wasm_bindgen]
@@ -18,22 +16,27 @@ extern "C" {
 
     #[wasm_bindgen(js_namespace = console, js_name = clog)]
     fn log2(a: JsValue);
+
+    #[wasm_bindgen(js_name = my_stringify)]
+    fn stringify(a: JsValue) -> JsValue;
 }
 
 macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
+#[derive(Default)]
 struct CallbackProcess {
     callback: js_sys::Function,
     handles: HashMap<u128, Handle>,
+    syscall_data_buffer: Option<SyscallData>,
 }
 
 impl CallbackProcess {
     pub fn new(callback: js_sys::Function) -> Self {
         Self {
             callback,
-            handles: HashMap::default(),
+            ..Default::default()
         }
     }
 }
@@ -42,39 +45,73 @@ impl Process for CallbackProcess {
     fn poll(&mut self, data: &kernel::SyscallData) -> kernel::PollResult<i64> {
         if let SyscallData::Handle(ref h) = data {
             let h = h.clone();
+            console_log!("Registering Handle {}", h.clone());
+            self.handles.insert(h.id, h);
+        }
+        if let SyscallData::Connection {
+            ref server,
+            ref client,
+        } = data
+        {
+            let h = server.clone();
+            console_log!("Registering Handle {}", h.clone());
+            self.handles.insert(h.id, h);
+
+            let h = client.clone();
+            console_log!("Registering Handle {}", h.clone());
             self.handles.insert(h.id, h);
         }
 
         let data = {
-            let r = serde_wasm_bindgen::to_value(data);
-            match r {
+            match serde_wasm_bindgen::to_value(&{
+                if let Some(d) = self.syscall_data_buffer.take() {
+                    d.clone()
+                } else {
+                    data.clone()
+                }
+            }) {
                 Ok(v) => v,
                 Err(_) => {
                     return kernel::PollResult::Done(-1);
                 }
             }
         };
+        // console_log!("Passing {:?}", stringify(data.clone()));
 
         let this = JsValue::null();
         let ret = self
             .callback
             .call1(&this, &data)
             .map(serde_wasm_bindgen::from_value::<PollResult<i64>>);
-        match ret {
+        let ret = match ret {
             Ok(Ok(x)) => match x {
                 PollResult::Syscall(Syscall::Send(h, d)) => {
-                    PollResult::Syscall(Syscall::Send(self.handles.get(&h.id).unwrap().clone(), d))
+                    if let Some(h) = self.handles.get(&h.id) {
+                        PollResult::Syscall(Syscall::Send(h.clone(), d))
+                    } else {
+                        self.syscall_data_buffer =
+                            Some(SyscallData::Fail(kernel::SyscallError::UnknownHandle));
+                        PollResult::Pending
+                    }
                 }
                 _ => x,
             },
             _ => PollResult::Done(-1),
-        }
+        };
+
+        // let r = serde_wasm_bindgen::to_value(&ret.clone());
+        // if let Ok(r) = r {
+        //      console_log!("Got {:?}", stringify(r));
+        // }
+
+        return ret;
     }
 }
 
 #[wasm_bindgen]
 pub struct Session {
     kernel: kernel::Kernel,
+    spawn_queue: Vec<Box<dyn Process>>,
 }
 
 #[wasm_bindgen]
@@ -84,6 +121,7 @@ impl Session {
     pub fn new() -> Self {
         Self {
             kernel: kernel::Kernel::default(),
+            spawn_queue: Vec::default(),
         }
     }
 
@@ -104,12 +142,16 @@ impl Session {
     } */
 
     pub fn add_process(&mut self, callback: js_sys::Function) -> JsValue {
-        self.kernel
-            .register_process(Box::new(CallbackProcess::new(callback)));
+        self.spawn_queue
+            .push(Box::new(CallbackProcess::new(callback)));
+
         JsValue::from_str("aiueo")
     }
 
     pub fn step(&mut self) {
+        for p in self.spawn_queue.drain(..) {
+            self.kernel.register_process(p);
+        }
         self.kernel.step();
     }
 }
