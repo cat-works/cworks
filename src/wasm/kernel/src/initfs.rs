e978a30c457a3f248558c4d3ff7c3f5f0fe82130
syoch <syoch64@gmail.com>
2023-02-19 08:08:40 +0900

    Update: some files

--- src/wasm/kernel/src/initfs.rs
+++ src/wasm/kernel/src/initfs.rs
@@ -2,10 +2,32 @@ use std::collections::HashMap;
 
 use crate::fs::{FSObj, RefOrVal};
 
+fn usr() -> FSObj {
+    let mut usr = HashMap::new();
+    usr.insert(
+        "mime".to_string(),
+        FSObj::Dist(RefOrVal::Val(HashMap::new())),
+    );
+    usr.insert(
+        "app".to_string(),
+        FSObj::Dist(RefOrVal::Val(HashMap::new())),
+    );
+    usr.insert(
+        "lib".to_string(),
+        FSObj::Dist(RefOrVal::Val(HashMap::new())),
+    );
+    FSObj::Dist(RefOrVal::Val(usr))
+}
+
 pub fn initfs() -> FSObj {
     let mut root = HashMap::new();
+    root.insert("usr".to_string(), usr());
     root.insert(
-        "usr".to_string(),
+        "mnt".to_string(),
+        FSObj::Dist(RefOrVal::Val(HashMap::new())),
+    );
+    root.insert(
+        "workspace".to_string(),
         FSObj::Dist(RefOrVal::Val(HashMap::new())),
     );
     FSObj::Dist(RefOrVal::Val(root))
