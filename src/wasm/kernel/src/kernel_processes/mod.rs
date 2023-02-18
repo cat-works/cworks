41afc7bb133f6c0f87f3409ee59113db9348321a
syoch <syoch64@gmail.com>
2023-02-18 15:55:05 +0900

    update: Added FS

--- src/wasm/kernel/src/kernel_processes/mod.rs
+++ src/wasm/kernel/src/kernel_processes/mod.rs
@@ -1,15 +1,17 @@
-use std::sync::{Arc, Mutex};
+use std::{sync::{Arc, Mutex}, ops::{Deref, DerefMut}};
 
 use crate::{fs::FSObj, rust_process::Session, SyscallData, SyscallError};
 
 enum FSReturns {
     InvalidCommandFormat,
+    UnsupportedMethod,
 }
 
 impl From<FSReturns> for String {
     fn from(value: FSReturns) -> Self {
         match value {
             FSReturns::InvalidCommandFormat => "InvalidCommandFormat".to_string(),
+            FSReturns::UnsupportedMethod => "UnsupportedMethod".to_string(),
         }
     }
 }
@@ -17,14 +19,40 @@ impl From<FSReturns> for String {
 enum FSCommand {
     List,
 }
+
 impl TryFrom<String> for FSCommand {
-    fn try_from(value: String) -> Result<Self> {
+    type Error = FSReturns;
+    fn try_from(value: String) -> Result<Self, FSReturns> {
         match value.as_str() {
-            "l" => Ok(FSCommand::List),
-            _ => panic!(),
+            "List" => Ok(FSCommand::List),
+            _ => Err(FSReturns::InvalidCommandFormat),
         }
     }
 }
+
+struct FS {
+    root: Arc<Mutex<FSObj>>,
+}
+
+impl FS {
+    pub fn new(root: Arc<Mutex<FSObj>>) -> Self {
+        Self { root }
+    }
+}
+
+impl Deref for FS {
+    type Target = Arc<Mutex<FSObj>>;
+    fn deref(&self) -> &Self::Target {
+        &self.root
+    }
+}
+
+impl DerefMut for FS {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.root
+    }
+}
+
 
 pub async fn fs_daemon_process(
     session: Arc<Session>,
@@ -37,20 +65,43 @@ pub async fn fs_daemon_process(
         let data = session.get_syscall_data().await;
         match data {
             SyscallData::Connection { client, server } => {
-                println!("Connection");
-                println!("  Client: {}", client);
-                println!("  Server: {}", server);
-
-                if server == s {
-                    sc = Option::Some(client);
-                }
+                // TODO: Handle this
             }
             SyscallData::ReceivingData { focus, data } if Option::Some(focus.clone()) == sc => {
+                let r = match FSCommand::try_from(data) {
+                    Ok(x) => x,
+                    Err(e) => {
+                        session.ipc_send(focus.clone(), e.into()).await?;
+                        continue;
+                    }
+                };
+
+                match r {
+                    FSCommand::List => {
+                        let daemon = daemon.lock().unwrap();
+                        let list = match daemon {
+                            FSObj::Dist(ref map) => {
+                                map
+                                    .keys()
+                                    .map(|x| x.to_string())
+                                    .collect::<Vec<_>>()
+                                    .join(", ")
+                                    .clone()
+                            },
+                            _ => {
+                                session
+                                .ipc_send(focus.clone(), "UnsupportedMethod".to_string())
+                                .await?;
+                                continue;
+                            }
+                        }
+                        session.ipc_send(focus.clone(), list).await?;
+                        continue;
+                    }
+                }
+
                 println!("Received: {} [Client -> Server]", data);
 
-                session
-                    .ipc_send(focus.clone(), "DAT-KITAYO".to_string())
-                    .await?;
                 break;
             }
             _ => {
