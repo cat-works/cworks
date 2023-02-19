4fb2fc7bbeb161feb5a91d78c9876f619f9edeb0
syoch <syoch64@gmail.com>
2023-02-19 10:51:00 +0900

    Updae: reduce some parts

--- src/wasm/wasm/src/lib.rs
+++ src/wasm/wasm/src/lib.rs
@@ -6,35 +6,15 @@ mod session;
 extern crate ghs_demangle;
 
 pub use generator::generate_user_id;
-use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
-
-#[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(a: &str);
-
-    #[wasm_bindgen(js_namespace = console, js_name = clog)]
-    fn log2(a: JsValue);
-
-    #[wasm_bindgen(js_name = my_stringify)]
-    fn stringify(a: JsValue) -> JsValue;
-}
-/*
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-} */
+use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn demangle_str(x: String) -> String {
     ghs_demangle::demangle(x).to_string()
 }
 
-#[wasm_bindgen(start)]
-fn m() {
-    // let s = serde_wasm_bindgen::to_value(&PollResult::<i64>::Syscall(Syscall::Send(
-    //     HandleIssuer::default().get_new_handle(0, HandleData::None),
-    //     "aiu".to_string(),
-    // )));
-    // log2(s.unwrap());
-    // console_log!("{:?}", s.unwrap());
+#[wasm_bindgen]
+pub fn init() {
+    console_error_panic_hook::set_once();
+    wasm_logger::init(wasm_logger::Config::default());
 }
--- src/wasm/wasm/src/processes/mod.rs
+++ src/wasm/wasm/src/processes/mod.rs
@@ -1,7 +1,3 @@
-mod ipc_master;
-mod ipc_slave;
 // mod python_interpreter;
 
-pub use ipc_master::IPCMaster;
-pub use ipc_slave::IPCSlave;
 // pub use python_interpreter::python_interpreter;
