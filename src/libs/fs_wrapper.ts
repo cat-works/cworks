28f61afbf84fc86801d3ad1bfff2122eb95b199b
syoch <syoch64@gmail.com>
2023-02-19 09:20:39 +0900

    update: Fix impl cd

---
+++ src/libs/fs_wrapper.ts
@@ -0,0 +1,52 @@
+import type { Handle, Process } from "./session";
+import { sleep } from "./utils";
+
+export class FileSystem {
+  public ipc: Handle;
+  constructor(p: Process) {
+    this.ipc = undefined;
+
+    let fs = this;
+    p.ipc_connect("system/file-system").then((ipc) => {
+      fs.ipc = ipc;
+    });
+  }
+
+  private handle_error(ret: string): void {
+    if (
+      [
+        "InvalidCommandFormat",
+        "UnsupportedMethod",
+        "InvalidHandle",
+        "UnknownPath",
+        "UnknownError",
+      ].includes(ret)
+    ) {
+      throw ret;
+    }
+  }
+
+  public async wait_for_ready(): Promise<void> {
+    while (this.ipc === undefined) {
+      await sleep(100);
+    }
+  }
+
+  public async cd(path: string): Promise<void> {
+    await this.ipc.send(`Cd?${path}`);
+    let ret = await this.ipc.recv();
+
+    this.handle_error(ret);
+
+    return;
+  }
+
+  public async list(): Promise<string[]> {
+    await this.ipc.send("List");
+    let ret = await this.ipc.recv();
+
+    this.handle_error(ret);
+
+    return ret.split("?");
+  }
+}
--- src/libs/index.ts
+++ src/libs/index.ts
@@ -1,5 +1,4 @@
 import init, { demangle_str } from "@src/wasm/pkg";
-export * from "./fs";
 
 export function ghs_demangle(str: string): string {
   return demangle_str(str);
