f4f04fa289925f64852fbc763dc8d6f4f66bd240
Yuki <yukikamome316@gmail.com>
2023-02-22 02:30:26 +0900

    update: added the main text for initialization

--- src/AppBar.svelte
+++ src/AppBar.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import app from "./libs/app";
 </script>
+<div class="container">
+  <div class="main-text">Cat Framework</div>
+  <div class="buttons">
 
-<div class="container" />
+  </div>
+</div>
 
 <style>
   .container {
@@ -10,8 +14,16 @@
     width: 100%;
     background-color: white;
     border: 1px solid lightgray;
-    border-radius: 3px;
+    border-radius: 2px;
 
     padding: 5px 10px;
+  }
+  .main-text {
+    /* UI Properties */
+    text-align: center;
+    font: normal normal bold 20px/29px Ubuntu;
+    letter-spacing: 0px;
+    color: #000000;
+    opacity: 1;
   }
 </style>
