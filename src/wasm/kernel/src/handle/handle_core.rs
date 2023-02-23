3ed5d30fdb778e20e40fdf2d11f8e4fb452597e4
syoch <syoch64@gmail.com>
2023-02-23 10:14:07 +0900

    Update: support deserialize for handlrecore

--- src/wasm/kernel/src/handle/handle_core.rs
+++ src/wasm/kernel/src/handle/handle_core.rs
@@ -1,11 +1,11 @@
 use super::HandleData;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct HandleCore {
     pub id: u128,
     pub pid: u128,
 
-    #[serde(skip_serializing)]
+    #[serde(skip_serializing, skip_deserializing)]
     pub(crate) data: HandleData,
 }
