9e2a7c9607e253db34ee688acf02a6f7438dc336
syoch <syoch64@gmail.com>
2023-06-30 23:37:51 +0900

    Update: Change UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,118 @@
 <script lang="ts">
-  import { app } from "./libs/app";
+  import Drawer, {
+    Content,
+    Header,
+    Title as DrawerTitle,
+    Subtitle,
+    AppContent,
+  } from "@smui/drawer";
+  import List, { Separator, Subheader } from "@smui/list";
+  import { H6 } from "@smui/common/elements";
+  import TopAppBar, { Section, Title } from "@smui/top-app-bar";
+  import IconButton from "@smui/icon-button";
 
-  import AppBar from "./AppBar.svelte";
+  import DrawerItem from "@components/DrawerItem.svelte";
+
+  import CafeCode from "./apps/wiiu/CafeCode.svelte";
+  import PNGExtractor from "./apps/tools/PNGExtractor.svelte";
+  import HexAdder from "./apps/tools/HexAdder.svelte";
+  import PPCDisassembler from "./apps/wiiu/PPCDisassembler.svelte";
+  import Introducation from "./apps/Introducation/index.svelte";
+  import Calc from "./apps/calc/index.svelte";
+  import Debug from "./apps/Debug.svelte";
+  import GHSDemangle from "./apps/tools/GHSDemangle.svelte";
+  import config from "./location_hash";
+
+  let active = config.app || localStorage.getItem("active_app") || "syoch";
+  $: localStorage.setItem("active_app", active);
+
+  let open = false;
 </script>
 
-<div style="height: 40px; width: 100%;"><AppBar /></div>
+<Drawer variant="dismissible" bind:open>
+  <Header>
+    <DrawerTitle>syoch's home page</DrawerTitle>
+    <Subtitle>nankairoiro</Subtitle>
+  </Header>
+
+  <Content>
+    <List>
+      <DrawerItem name="syoch" icon="contact_mail" bind:active bind:open />
+      <Separator />
+
+      <Subheader component={H6}>Converters</Subheader>
+      <DrawerItem
+        name="Cafecode Converter"
+        icon="sports_esports"
+        bind:active
+        bind:open
+      />
+      <DrawerItem
+        name="PowerPC Disassembler"
+        icon="undo"
+        bind:active
+        bind:open
+      />
+
+      <Separator />
+
+      <Subheader component={H6}>Tools</Subheader>
+      <DrawerItem
+        name="PNG Extractor"
+        icon="filter_list"
+        bind:active
+        bind:open
+      />
+      <DrawerItem name="GHS Demangle" icon="undo" bind:active bind:open />
+      <DrawerItem name="HEX Adder" icon="hexagon" bind:active bind:open />
+      <DrawerItem name="Calculator" icon="hexagon" bind:active bind:open />
+    </List>
+  </Content>
+</Drawer>
+
+<AppContent class="app-content">
+  <TopAppBar variant="static" class="app-bar">
+    <Section>
+      <IconButton
+        class="material-icons"
+        on:click={() => {
+          open = !open;
+        }}>menu</IconButton
+      >
+      <Title>{active}</Title>
+    </Section>
+  </TopAppBar>
+
+  <main class="main-content">
+    {#if active == "Cafecode Converter"}
+      <CafeCode />
+    {:else if active == "PowerPC Disassembler"}
+      <PPCDisassembler />
+    {:else if active == "PNG Extractor"}
+      <PNGExtractor />
+    {:else if active == "HEX Adder"}
+      <HexAdder />
+    {:else if active == "syoch"}
+      <Introducation />
+    {:else if active == "Calculator"}
+      <Calc />
+    {:else if active == "GHS Demangle"}
+      <GHSDemangle />
+    {:else if active == "Debug"}
+      <Debug />
+    {/if}
+  </main>
+</AppContent>
+
+<style>
+  :global(.app-content) {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .main-content {
+    padding: 16px;
+    flex: 1;
+  }
+</style>
