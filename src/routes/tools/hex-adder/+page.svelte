603e3558859ae2236400046a0ef5c485a71fa7b3
syoch <syoch64@gmail.com>
2025-05-29 22:47:43 +0900

    Update: Fixed some related firebase things

--- src/routes/tools/hex-adder/+page.svelte
+++ src/routes/tools/hex-adder/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import Textfield from "@smui/textfield";
-
   let inp = "00000000";
   let diff = "";
 
@@ -12,7 +10,7 @@
   $: localStorage.setItem("diff", diff);
 </script>
 
-<Textfield bind:value={inp} label="Input" /><br />
+<textarea bind:value={inp} placeholder="Input"></textarea><br />
 added value: {(parseInt(inp, 16) + parseInt(diff, 16)).toString(16)}<br />
 
-<Textfield bind:value={diff} label="A value to add" />
+<textarea bind:value={diff} placeholder="A value to add"></textarea>
