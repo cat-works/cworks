9cb4ec027785b435a1ee2898bd4c83cfb94071d9
syoch <syoch64@gmail.com>
2025-06-07 16:04:51 +0000

    a

--- src/routes/debug/debug/test_proc.lua
+++ src/routes/debug/debug/test_proc.lua
@@ -3,17 +3,15 @@ local cworks = require("cworks");
 local io_handle = cworks.ipc_connect("system/stdio/root", function() end)
 
 local fs_handle = cworks.ipc_connect("system/file-system", function(text)
-  cworks.send(io_handle, "File system response: " .. text)
+  cworks.send(io_handle, "File system response: " .. text .. "\n")
 end)
 
 cworks.send(io_handle, "fs_handle: " .. fs_handle .. "\n")
 cworks.send(fs_handle, "List?/")
 
 for j = 1, 10, 1 do
-  for i = 1, 10, 1 do
-    cworks.sleep(0.1)
-  end
-  cworks.send(io_handle, "j = " .. j)
+  cworks.sleep(0.1)
+  cworks.send(io_handle, "j = " .. j .. "\n")
 end
 
 cworks.send(io_handle, "Exiting\n")
